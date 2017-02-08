3755db6964f03b39734695729ec04b32d2107753
soceanainn <seamus.o-ceanainn@ucdconnect.ie>
2017-02-08 03:47:28 +0000

    Changes
    Comments, width /height now set by constants in index.html, app.js
    simplified

--- app.js
+++ app.js
@@ -10,55 +10,11 @@ app.use('/client',express.static(__dirname + '/client'));
 serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
 
-var SOCKET_LIST = {};
-function Player (username, id){
-	this.username = username;
-	this.id = id;
-}
-playerList = [];
-Player.onConnect = function(socket,username){
-	var player = Player({
-		username:username,
-		id:socket.id,
-	});/*
-	socket.on('keyPress',function(data){
-		if(data.inputId === 'left')
-			player.pressingLeft = data.state;
-		else if(data.inputId === 'right')
-			player.pressingRight = data.state;
-		else if(data.inputId === 'up')
-			player.pressingUp = data.state;
-		else if(data.inputId === 'down')
-			player.pressingDown = data.state;
-		else if(data.inputId === 'attack')
-			player.pressingAttack = data.state;
-		else if(data.inputId === 'mouseAngle')
-			player.mouseAngle = data.state;
-	});
-	
-	socket.on('sendMsgToServer',function(data){
-		for(var i in SOCKET_LIST){
-			SOCKET_LIST[i].emit('addToChat',player.username + ': ' + data);
-		}
-	});
-	
-	socket.on('sendPmToServer',function(data){ //data:{username,message}
-		var recipientSocket = null;
-		for(var i in Player.list)
-			if(Player.list[i].username === data.username)
-				recipientSocket = SOCKET_LIST[i];
-		if(recipientSocket === null){
-			socket.emit('addToChat','The player ' + data.username + ' is not online.');
-		} else {
-			recipientSocket.emit('addToChat','From ' + player.username + ':' + data.message);
-			socket.emit('addToChat','To ' + data.username + ':' + data.message);
-		}
-	});*/
-}
+var SOCKET_LIST = [];
+var playerList = [];
 
-Player.onDisconnect = function(socket){
-	delete playerList[socket];
-	
+disconnectUser = function(socket){
+	delete playerList[socket.id];
 }
 
 function isUsernameTaken(name){
@@ -66,13 +22,13 @@ function isUsernameTaken(name){
 	else return(true);
 }
 function addUser(name, socket){
-	playerList[socket] = name;
+	playerList[socket.id] = name;
 }
 
 var io = require('socket.io')(serv,{});
 
 io.sockets.on('connection', function(socket){
-	socket.id = Math.random();
+	socket.id = Math.floor(Math.random()*100);
 	SOCKET_LIST[socket.id] = socket;
 	
 	socket.on('signIn',function(username){
@@ -80,30 +36,20 @@ io.sockets.on('connection', function(socket){
 				socket.emit('signInResponse',{success:false});		
 		} else {
 			addUser(username, socket);
-			socket.emit('signInResponse',{success:true});					
+			socket.emit('signInResponse',{success:true});
 		}		
 	});
 	
-	
+	socket.on('sendMsgToServer',function(data){
+		for(var i in SOCKET_LIST){
+			var str = playerList[socket.id] + ': ' + data;
+			console.log(str);
+			SOCKET_LIST[i].emit('addToChat',str);
+		}
+	});
+
 	socket.on('disconnect',function(){
 		delete SOCKET_LIST[socket.id];
-		Player.onDisconnect(socket);
+		disconnectUser(socket);
 	});
 });
-
-setInterval(function(){
-	var pack = {
-	}
-	
-	for(var i in SOCKET_LIST){
-		var socket = SOCKET_LIST[i];
-		//socket.emit('init',initPack);
-	}
-},1000/25);
-
-
-
-
-
-
-
