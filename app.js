450971670bbde632eb795fcd7c9421f7bc64e7d3
soceanainn <seamus.o-ceanainn@ucdconnect.ie>
2017-02-21 23:04:33 +0000

    Debugging

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
 app.get('/',function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
