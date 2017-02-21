79800aca1a8c408a2d0c4f1b3161671029eed4f0
soceanainn <seamus.o-ceanainn@ucdconnect.ie>
2017-02-21 22:45:17 +0000

    OpenShift Configuration
    Configured to run on OpenShift

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.get('/',function(req, res) {
 });
 app.use('/client',express.static(__dirname + '/client'));
 
-serv.listen(process.env.PORT || 2000);
+serv.listen(process.env.OPENSHIFT_NODEJS_PORT || 2000);
+//serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
 
 //-----------------------------------------------------
