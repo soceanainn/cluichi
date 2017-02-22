5f233cbc8379c58dc369158c1b7ef5c7d4bad822
soceanainn <seamus.o-ceanainn@ucdconnect.ie>
2017-02-22 00:25:50 +0000

    Reconfiguration

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ var app = express();
 var server = require('http').Server(app);
 
 // Settings for OpenShift or Local Machine
-var port = process.env.OPENSHIFT_NODEJS_PORT; //|| 8080;
-var ip = process.env.OPENSHIFT_NODEJS_IP; //|| '127.0.0.1';
-server.listen(port, ip, function() {
-  console.log("Server running @ http://" + ip + ":" + port);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
 
 app.get('/',function(req, res) {
