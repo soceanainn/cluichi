724def91fa5f17fa5a85cc02e2ca4e7cdae32987
soceanainn <seamus.o-ceanainn@ucdconnect.ie>
2017-02-22 03:03:44 +0000

    Abandoned OpenShift
    Changing to different online provider

--- app.js
+++ app.js
@@ -18,9 +18,8 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 
-// Settings for OpenShift or Local Machine
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.PORT || 8080
+var server_ip_address = process.env.IP || '127.0.0.1'
 
 server.listen(server_port, server_ip_address, function(){
   console.log("Listening on " + server_ip_address + ", server_port " + server_port)
