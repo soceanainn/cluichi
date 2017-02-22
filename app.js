bea49a4f80d59feb08d214dfc1d0e035faca7f9c
soceanainn <seamus.o-ceanainn@ucdconnect.ie>
2017-02-22 03:26:33 +0000

    Configuration for Heroku
    Trying to bind the port

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 
+server.listen(process.env.PORT || 2000);
+
 app.get('/',function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
